7f3a2e5feae6bcf657eb1218a39a95e6b8d1d815
Phil Neiman <pneiman1@gmail.com>
2017-05-16 21:15:10 -0700

    Add project files.

---
+++ baseballapp3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(baseballapp3.Startup))]
+namespace baseballapp3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
